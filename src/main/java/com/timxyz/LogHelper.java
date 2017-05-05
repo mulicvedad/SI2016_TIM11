51fd00fdb0421bae7dea151f6f70e20eeeef7a70
muhamedparic <mparic1@etf.unsa.ba>
2017-05-05 19:12:17 +0200

    Mozda ovo popravi?

--- src/main/java/com/timxyz/LogHelper.java
+++ src/main/java/com/timxyz/LogHelper.java
@@ -1,6 +1,7 @@
 package com.timxyz;
 
 import com.timxyz.models.AccessLog;
+import com.timxyz.models.Account;
 import com.timxyz.repositories.AccessLogRepository;
 import com.timxyz.services.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,14 +16,14 @@ public class LogHelper {
     @Autowired
     private static AccessLogRepository accessLogRepository;
 
-    public static void log(Long userId, String type, String description, String tableName) {
+    public static void log(Account account, String type, String description, String tableName) {
         AccessLog accessLog = new AccessLog();
 
         accessLog.setDate(Timestamp.valueOf(LocalDateTime.now()));
         accessLog.setType(type);
         accessLog.setDescription(description);
         accessLog.setTableName(tableName);
-        accessLog.setAccount(new AccountService().get(userId));
+        accessLog.setAccount(account);
 
         accessLogRepository.save(accessLog);
     }

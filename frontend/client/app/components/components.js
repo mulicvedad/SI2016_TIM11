0f9a26dbb1fb087c73fc80e2f844c8c1f11b9afc
Sanil Musić <music.sanil@gmail.com>
2017-05-17 19:02:29 +0200

    more cleaning up and basis of the new layout

--- frontend/client/app/components/components.js
+++ frontend/client/app/components/components.js
@@ -7,6 +7,7 @@ import Categories from './categories/categories';
 import Status from './status/status';
 import Items from './items/items';
 import ItemsCRD from './itemsCRD/itemsCRD';
+import Navbar from './navbar/navbar';
 
 let Components = angular.module('app.components', [
   Home,
@@ -16,7 +17,8 @@ let Components = angular.module('app.components', [
   Status,
   LocationTypes,
   Items,
-  ItemsCRD    
+  ItemsCRD,
+  Navbar
 ])
 .name;
 

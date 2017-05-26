2a6afcb40d2e3d7c9eb3264abd210e619751cd82
enafly <emuratspah1@etf.unsa.ba>
2017-05-26 11:36:26 +0200

    Dizajn sala i malo dodavanje..

--- src/main/java/com/timxyz/repositories/LocationRepository.java
+++ src/main/java/com/timxyz/repositories/LocationRepository.java
@@ -1,12 +1,19 @@
 package com.timxyz.repositories;
 
-
 import com.timxyz.models.Location;
+import java.util.Collection;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.repository.query.Param;
+
 /**
  * Created by ramic on 04.05.2017..
  */
 public interface LocationRepository extends PagingAndSortingRepository <Location, Long> {
+    
     Location findFirstByName(String name);
+
+    @Query("select l from Location l where l.name like %:name%")
+    public Collection<Location> filterByName(@Param("name") String name);
+
 }
